85f57e3e8c512b3717cdb8ac8d1b353fe930a6d2
Mashopy <eliasgheeraert@gmail.com>
2026-02-27 15:31:51 +0100

    RMX2020: ImsService: Don't include mediatek-{common,framework} as a shared lib
    The stub is a boot jar now
    
    * Also We building mediatek-common stub from source [1]
    [1]: crdroidandroid/android_device_realme_RMX2020@a2163aae278d105c3e8fee32471dabc8a0992d20
    
    * ImsService.apk updated from [2]
    [2]: https://github.com/Koushikdey2003/packages_apps_ImsService/commit/7788755010903518c4f525648e57c59f6be9a797
    
    Change-Id: I2906eb6e31378b5c58da5c9b54c05bd6198b2639

--- RMX2020-ims.mk
+++ RMX2020-ims.mk
@@ -1,9 +1,7 @@
 PRODUCT_PACKAGES += \
     ImsService \
-    mediatek-common \
-    mediatek-framework \
     mediatek-ims-base \
     mediatek-ims-common \
     mediatek-telecom-common \
     mediatek-telephony-base \
-    mediatek-telephony-common \
+    mediatek-telephony-common
