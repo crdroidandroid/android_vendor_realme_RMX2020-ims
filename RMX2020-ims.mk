5f5781c3aca457172cd01a8836eb31ba062eb56c
Giovanni Ricca <giovanniricca@proton.me>
2026-08-23 00:58:12 +0530

    Revert "RMX2020: Switch to shared libs to MediaTek framework jars"
    Reason: We no longer need that
    
    This reverts commit b437b0a4c325c7c7e5f7c456b563ffaf7f602bc1.
    
    Change-Id: I8bcda97b830408777c5bb7de86a73d04ed8ac4b5

--- RMX2020-ims.mk
+++ RMX2020-ims.mk
@@ -5,3 +5,10 @@ PRODUCT_PACKAGES += \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common
+
+PRODUCT_BOOT_JARS += \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
